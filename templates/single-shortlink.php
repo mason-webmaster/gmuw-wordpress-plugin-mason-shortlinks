eecf6cf7d459c9d423655ccc4a0576b514adf837
Jan Macario <jmacario@gmu.edu>
2024-02-05 21:59:26 -0500

    Removes redirect from shortlink single post template.

--- templates/single-shortlink.php
+++ templates/single-shortlink.php
@@ -3,4 +3,3 @@
 <p>Shortlink text: <?php echo $post->shortlink_slug; ?></p>
 <p>Target URL: <?php echo $post->shortlink_target_url; ?></p>
 <p>Approved?: <?php echo $post->shortlink_approved; ?></p>
-<?php wp_redirect( get_post()->shortlink_target_url, 301 ); exit; ?>
