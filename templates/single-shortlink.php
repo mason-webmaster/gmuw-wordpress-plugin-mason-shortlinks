3512d31aceeda3341d405beb07b7ec5b047debf3
Jan Macario <jmacario@gmu.edu>
2026-01-27 12:21:57 -0500

    Complete rewrite of plugin, focusing on building upon the existing third-party Redirection plugin.

--- templates/single-shortlink.php
+++
@@ -1,5 +0,0 @@
-<h3>Shortlink</h3>
-<p>ID: <?php echo $post->ID; ?></p>
-<p>Shortlink text: <?php echo $post->shortlink_slug; ?></p>
-<p>Target URL: <?php echo $post->shortlink_target_url; ?></p>
-<p>Approved?: <?php echo $post->shortlink_approved; ?></p>
