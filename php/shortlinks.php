55d1b4e256889b414b46e3292fd4bd6a9df609d0
Jan Macario <jmacario@gmu.edu>
2026-04-21 16:59:00 -0400

    Adds additional styling to the target URL field on the shortlink listing page.

--- php/shortlinks.php
+++ php/shortlinks.php
@@ -67,7 +67,7 @@ function gmuw_sl_shortlinks_table($redirects,$compact=false){
 				//label
 				$return_value.='<td>'.'<a style="font-weight:bold;" title="'.$shortlink_url.'" href="'.$view_url.'">'.$shortlink_url_display.'</a> '.$copy_link.'</td>';
 				//target
-				$return_value.='<td style="max-width:40em;">'.'<a title="'.$target_url.'" href="'.$target_url.'" target="_blank">'.$target_url_display.'</a>'.'</td>';
+				$return_value.='<td style="max-width:40em;">'.'<a style="word-break:break-all;" title="'.$target_url.'" href="'.$target_url.'" target="_blank">'.$target_url_display.'</a>'.'</td>';
 				//user
 				$return_value.='<td>'.$redirect_user.'</td>';
 				//group
