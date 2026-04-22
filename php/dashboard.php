347a82f580907c5747d29c672c64fea2a5b25139
Jan Macario <jmacario@gmu.edu>
2026-04-22 19:17:38 -0400

    Removes the 'recent' and 'top' shortlinks dashboard meta boxes as they are of questionable utility, and that data is better represented in the full shortlink display.

--- php/dashboard.php
+++ php/dashboard.php
@@ -24,10 +24,10 @@ function gmuw_sl_custom_dashboard_meta_boxes() {
   add_meta_box("gmuw_sl_custom_dashboard_meta_box_shortlinks_all", "All Shortlinks", "gmuw_sl_custom_dashboard_meta_box_shortlinks_all", "dashboard","normal");
 
    /* top shortlinks */
-  add_meta_box("gmuw_sl_custom_dashboard_meta_box_shortlinks_top", "Top Shortlinks", "gmuw_sl_custom_dashboard_meta_box_shortlinks_top", "dashboard","normal");
+  //add_meta_box("gmuw_sl_custom_dashboard_meta_box_shortlinks_top", "Top Shortlinks", "gmuw_sl_custom_dashboard_meta_box_shortlinks_top", "dashboard","normal");
 
    /* new shortlinks */
-  add_meta_box("gmuw_sl_custom_dashboard_meta_box_shortlinks_new", "Recently Added Shortlinks", "gmuw_sl_custom_dashboard_meta_box_shortlinks_new", "dashboard","normal");
+  //add_meta_box("gmuw_sl_custom_dashboard_meta_box_shortlinks_new", "Recently Added Shortlinks", "gmuw_sl_custom_dashboard_meta_box_shortlinks_new", "dashboard","normal");
 
    /* your shortlinks */
   add_meta_box("gmuw_sl_custom_dashboard_meta_box_shortlinks_current_user", "Your Shortlinks", "gmuw_sl_custom_dashboard_meta_box_shortlinks_current_user", "dashboard","normal",);
